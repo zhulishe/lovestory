45c82347b75a7770b641aafc2724a959b926c383
lisa <zhulishe@qq.com>
2015-08-30 15:44:32 +0800

    change models.py(class Article,Comment)

--- misslove/models.py
+++ misslove/models.py
@@ -1,3 +1,27 @@
+# -*- coding: UTF-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Article(models.Model):
+	article_type = (
+		(1,u'失恋'),
+		(2,u'暗恋'),
+		(3,u'异地恋'),
+		(4,u'爱恋'),
+	)
+	author = models.ForeignKey(User)
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	choose_type = models.IntegerField(choices=article_type)
+	created_time = models.TimeField()
+	# article valid or invalid
+	status = models.IntegerField()
+	image = models.ImageField()
+
+class Comment(models.Model):
+	author = models.ForeignKey(User)
+	content = models.TextField()
+	comment_time = models.TimeField()
+	# comment is valid or invalid
+	status = models.IntegerField()
