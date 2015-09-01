bd843eb3acffaa7aed59412d6004414c53316c0e
donald <chenyvehtung@gmail.com>
2015-09-01 15:34:40 +0800

    fix merge error

---
+++ misslove/migrations/0005_auto_20150901_1222.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('misslove', '0004_auto_20150831_1830'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='article',
+            options={'verbose_name': '\u6587\u7ae0'},
+        ),
+    ]
