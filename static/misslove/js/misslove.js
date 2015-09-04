fcf7f8dcb8b6b70a7f5317b4f27735eabdfe95a5
donald <chenyvehtung@gmail.com>
2015-09-04 12:33:17 +0800

    update homepage.html

---
+++ static/misslove/js/misslove.js
@@ -0,0 +1,14 @@
+/**
+ * Created by donald on 15-9-4.
+ */
+$(document).ready(function() {
+
+    /*************首页(homepage.html)*****************/
+    $(".carousel").carousel({interval: 3000}).carousel('cycle');
+
+
+
+
+
+
+});
